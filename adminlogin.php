88a43851c184802eae3c11765a777b73d30b0a4f
jeaniameiraguha <jeaniameiraguha@gmail.com>
2024-09-11 15:14:02 +0200

    initial commit

--- adminlogin.php
+++ adminlogin.php
@@ -6,6 +6,22 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="" method="post"></form>
+    <form action="" method="post">
+        <input type="text" placeholder="Admin Name" name="admin_name"><br><br>
+        <input type="text" placeholder="Admin password" name="admin_password"> <br><br>
+        <button name="submit">Login</button>
+    </form>
 </body>
 </html>
+<?php
+
+include('conn.php');
+$admin_name='Iraguha';
+$admin_password='dot2024';
+if ($admin_password !='dot2024') {
+    # code...
+    echo "wrong credentials";
+}
+
+
+?>
