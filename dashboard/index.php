e044d7dfed09d0c6b112083f30192d5c151b8f42
jeaniameiraguha <jeaniameiraguha@gmail.com>
2024-09-11 13:51:28 +0200

    initial commit

---
+++ dashboard/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  <h2 class="text-center my-4 container">WELCOME TO USER MANAGEMENT SYSTEM</h2>
+</body>
+</html>
