7eeec62502c44bd5ea85550f4ad6e980c33ea31a
jeaniameiraguha <jeaniameiraguha@gmail.com>
2024-09-11 12:43:59 +0200

    initial commit

--- add.php
+++ add.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+  <link rel="stylesheet" href="index.css">
 <link rel="stylesheet" href="css/bootstrap.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Add New</title>
 </head>
-<body style="background-color: black;">
+<body style="background-color: #28f0dfa2;">
+  <h2 class="text-center bg-dark card card-header" >Add New User</h2>
   <form action="insert.php" method="post" class="form was-validated container  mt-5 p-5 lg-6 ">
 
     <input type="text" name="User_name" required class="form-control mb-3 mx-0"  placeholder="User_name"> 
