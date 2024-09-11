0a77f9824b9b3dacd1739bf901919b01375cd3c0
jeaniameiraguha <jeaniameiraguha@gmail.com>
2024-09-11 12:01:03 +0200

    initial commit

--- insert.php
+++ insert.php
@@ -7,20 +7,21 @@ if (isset($_POST['submit'])) {
     $User_name=$_POST['User_name'];
     $User_Address=$_POST['User_Address'];
     $User_Phone=$_POST['User_Phone'];
+    
    // $User_Phone=$_POST['User_Phone'];
-   if (empty($User_name) || empty($User_Address)  || empty($User_Phone)) {
-    # code...
-    echo "<script>alert('Plz fill this field')</script>";
-   }
+   
    $insert = mysqli_query($conn , "INSERT INTO users VALUES('' , '$User_name' , '$User_Address' , '$User_Phone')");
 
    if ($insert) {
     # code...
+    
 header('location:select.php');
+
    }
    else{
     echo "something went wrong";
    }
+   
 
 }
 
