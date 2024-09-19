ee93709321729c5e27583cc521529dfb6f9a9d7e
jeanaimeiraguha <jeanaimeiraguha@gmail.com>
2024-09-19 11:50:40 +0200

    updated

--- SESSION/login.php
+++ SESSION/login.php
@@ -12,5 +12,6 @@ if(isset($_POST['submit'])) {
     $User_name=$_POST['User_name'];
     $User_Address=$_POST['User_Address'];
     $User_Phone=$_POST['User_Phone'];
+    $select=mysqli_query($conn , "SELECT * FROM  USERS where User_name='$User_name' , User_address='$User_Address' AND User_Phone='$User_Phone'")
 }
 ?>
