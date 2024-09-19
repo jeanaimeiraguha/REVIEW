1848bb6876934654edf465ed8233c176187f8061
jeanaimeiraguha <jeanaimeiraguha@gmail.com>
2024-09-19 11:44:01 +0200

    updated

--- SESSION/login.php
+++ SESSION/login.php
@@ -2,4 +2,12 @@
 User_name <input type="text" name="User_name">
 User_Address <input type="text" name="User_Address">
 User_Phone <input type="text" name="Use_phone">
+<button name="submit">Login</button>
 </form>
+<?php
+include('conn.php');
+session_start()
+if (isset()) {
+    # code...
+}
+?>
