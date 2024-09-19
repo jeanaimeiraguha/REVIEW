a7d8652c74c10b5a7d5b277f743cf6d715f1837f
jeanaimeiraguha <jeanaimeiraguha@gmail.com>
2024-09-19 11:42:26 +0200

    updated

---
+++ SESSION/login.php
@@ -0,0 +1,5 @@
+<form action="" method="post">
+User_name <input type="text" name="User_name">
+User_Address <input type="text" name="User_Address">
+User_Phone <input type="text" name="Use_phone">
+</form>
